4c6f46d5c04b432674f5436009bb1350a153f1d0
Frederik Munk Madsen <frederik@os-madsen.dk>
2021-11-29 13:53:52 +0100

    Debugging work

--- FastCircuit.py
+++ FastCircuit.py
@@ -7,6 +7,7 @@ from enum import Enum
 from bitarray import bitarray
 from bitarray.util import int2ba, ba2int
 
+
 class GateType(Enum):
     NOT = 0,
     AND = 1,
@@ -18,18 +19,20 @@ class GateType(Enum):
 #SIZE = 128
 #MAGIC = 2863311530 # 101010101010101010... in binary
 
-SIZE = 4
-MAGIC = 5
+SIZE = 8
+MAGIC = 0
 EXTENDED_SIZE = SIZE + 1
 
 
 def make_bitarray(i: int):
     return int2ba(i, SIZE, endian='little')
 
+
 def make_bitarray_with(i: int, b: int):
     r = make_bitarray(i)
     r.append(b)
     return r
+
 
 def zero(b: bitarray):
     for bit in b:
@@ -100,12 +103,20 @@ class GarbledGate(CircuitGate):
             # 1. Output permutation bit
             permutation = self.Left.Permutation ^ self.Right.Permutation
 
+            if(permutation == 1):
+                print("\n\nBAD???????\n\n")
+
             # 2. Translated keys
             ki0 = F(self.Left.K[0], make_bitarray_with(self.Index, self.Left.Permutation))[:SIZE]
             ki1 = F(self.Left.K[1], make_bitarray_with(self.Index, 1 ^ self.Left.Permutation))[:SIZE]
 
+            print(str(self.Index) + "] ki0 -- " + str(ki0))
+            print(str(self.Index) + "] ki1 -- " + str(ki1))
+
             # 3. New offset
             delta = ki0 ^ ki1
+
+            print(str(self.Index) + "] d   -- " + str(delta))
 
             # 4. Translated keys for j (right)
             if self.Right.Permutation == 0:
@@ -117,6 +128,9 @@ class GarbledGate(CircuitGate):
                 kj0 = kj1 ^ delta
                 T = F(self.Right.K[0], make_bitarray_with(self.Index, 1))[:SIZE] ^ kj0
 
+            print(str(self.Index) + "] kj0 -- " + str(kj0))
+            print(str(self.Index) + "] kj1 -- " + str(kj1))
+
             # 5. Compute keys for output wire l
             k0 = ki0 ^ kj0
             k1 = k0 ^ delta
@@ -125,9 +139,14 @@ class GarbledGate(CircuitGate):
             self.Permutation = permutation
             self.K = [k0, k1]
             self.TXor = T
+
+            print(str(self.Index) + "] pi  -- " + str(self.Permutation))
+            print(str(self.Index) + "] k0  -- " + str(self.K[0]))
+            print(str(self.Index) + "] k1  -- " + str(self.K[1]))
+            print(str(self.Index) + "] T   -- " + str(self.TXor))
+
         else:
             print("Cannot garble a " + str(self.Op) + " gate!")
-
 
     def Evaluate(self):
         if self.Op == GateType.XOR:
@@ -138,8 +157,14 @@ class GarbledGate(CircuitGate):
             else:
                 k = vLeft ^ vRight
 
+            print(str(self.Index) + "] vL  -- " + str(vLeft))
+            print(str(self.Index) + "] vR  -- " + str(vRight))
+            print(str(self.Index) + "] k   -- " + str(k))
+
             self.Output = k
             self.Signal = self.Left.Signal ^ self.Right.Signal
+
+            print(str(self.Index) + "] sig -- " + str(self.Signal))
         else:
             print("Cannot evaluate a " + str(self.Op) + " gate!")
 
@@ -154,7 +179,11 @@ class InputGate(CircuitGate):
         k0, k1 = pick_random_pair()
         self.K = [k0, k1]
 
+        print(str(self.Index) + "] k0  -> " + str(self.K[0]))
+        print(str(self.Index) + "] k1  -> " + str(self.K[1]))
+
         self.Permutation = secrets.randbits(1)
+        print(str(self.Index) + "] pi  -> " + str(self.Permutation))
 
         e0 = k0.copy()
         e0.append(self.Permutation)
@@ -162,14 +191,19 @@ class InputGate(CircuitGate):
         e1.append(1 ^ self.Permutation)
         self.E = [e0, e1]
 
+        print(str(self.Index) + "] e0  -> " + str(self.E[0]))
+        print(str(self.Index) + "] e1  -> " + str(self.E[1]))
 
     def Encode(self, value):
         self.Output = self.E[value]
-
+        print(str(self.Index) + "] in  -> " + str(self.Output))
 
     def Evaluate(self):
         self.Signal = self.Output[SIZE]
         self.Output = self.Output[:SIZE]
+
+        print(str(self.Index) + "] sig -> " + str(self.Signal))
+        print(str(self.Index) + "] out -> " + str(self.Output))
         return
 
 
@@ -180,15 +214,29 @@ class OutputGate:
 
     def Garble(self):
         g = self.Value
-        self.D[0] = F(g.K[g.Permutation], make_bitarray_with(g.Index, g.Permutation))
-        self.D[1] = F(g.K[1 ^ g.Permutation], make_bitarray_with(g.Index, 1 ^ g.Permutation))
-        return
+        #self.D[0] = F(g.K[g.Permutation], make_bitarray_with(g.Index, g.Permutation))
+        #arr = make_bitarray_with(g.Index, 1 ^ g.Permutation)
+        #self.D[1] = F(g.K[1 ^ g.Permutation], arr)
+        # TODO: Changing it to this seems to fix the issue, but I do not think this is correct!
+        self.D[0] = F(g.K[0], make_bitarray_with(g.Index, g.Permutation))
+        arr = make_bitarray_with(g.Index, 1 ^ g.Permutation)
+        self.D[1] = F(g.K[1], arr)
+        print("X] d0  <- " + str(self.D[0]))
+        print("X] d1  <- " + str(self.D[1]))
 
+        print("Evaluating garble:")
+        print("> " + str(g.K[1 ^ g.Permutation]))
+        print("> " + str(arr))
 
     def Evaluate(self):
-        output = F(self.Value.Output, make_bitarray_with(self.Value.Index, self.Value.Signal))
+        print("Evaluating output:")
+        print("> " + str(self.Value.Output))
+        arr = make_bitarray_with(self.Value.Index, self.Value.Signal)
+        print("> " + str(arr))
+
+        output = F(self.Value.Output, arr)
         self.Value.Output = output
-        return
+        print("X] out <- " + str(self.Value.Output))
 
     def Decode(self):
         g = self.Value
@@ -209,18 +257,14 @@ class FastCircuit:
         self.Intermediates = intermediates
 
     def Garble(self):
-        for gate in self.Inputs:
-            gate.Garble()
         for gate in self.Gates:
             gate.Garble()
         for gate in self.Outputs:
             gate.Garble()
 
-
     def Encode(self, values):
         for i, gate in enumerate(self.Inputs):
             gate.Encode(values[i])
-
 
     def Evaluate(self):
         for gate in self.Inputs:
@@ -229,7 +273,6 @@ class FastCircuit:
             gate.Evaluate()
         for gate in self.Outputs:
             gate.Evaluate()
-
 
     def Decode(self):
         result = []
@@ -253,10 +296,17 @@ all = [input1, input2, xorGate]
 circuit = FastCircuit(all, ins, outs, steps)
 
 # Garble
+a = 1
+b = 0
+print("##### Garble")
 circuit.Garble()
-circuit.Encode([0, 0])
+print("##### Encode")
+circuit.Encode([a, b])
+print("##### Evaluate")
 circuit.Evaluate()
+print("##### Decode")
 result = circuit.Decode()
 
 print(result)
-print("FastCircuit done!")
+correct = (a ^ b) == result[0]
+print("FastCircuit done! " + str(correct))
